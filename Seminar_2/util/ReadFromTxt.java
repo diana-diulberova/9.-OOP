6431174cbfeefc0a204c637017fed7ed83ed8205
Diana <d.dyulberova@gmail.com>
2023-02-01 19:42:58 +0500

    Homework_2

--- Seminar_2/util/ReadFromTxt.java
+++ Seminar_2/util/ReadFromTxt.java
@@ -2,12 +2,23 @@ package Seminar_2.util;
 
 import java.util.List;
 
+import Seminar_2.data.Student;
+import Seminar_2.data.Teacher;
+
 public class ReadFromTxt {
     public static Seminar_2.data.Teacher getTeacherFromTxt() {
         return null;
     }
 
     public static List<Seminar_2.data.Student> getStudentsFromTxt() {
+        return null;
+    }
+
+    public static Teacher readTeacher(int groupNumber) {
+        return null;
+    }
+
+    public static List<Student> readStudents(int groupNumber) {
         return null;
     }
 }
